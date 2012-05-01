a181ade609677c5e0dac310977da2540f8f95b4f
Randall Degges <rdegges@gmail.com>
2012-04-30 22:56:15 -0700

    Adding a skeleton setup.py.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from os.path import abspath, dirname, join, normpath
+
+from setuptools import setup
+
+
+setup(
+
+    # Basic package information:
+    name = 'django-heroku-postgresify',
+    version = '0.1',
+    packages = ('postgresify',),
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies:
+    install_requires = ['Django>=1.2', 'dj-database-url==0.1.2'],
+
+    # Metadata for PyPI:
+    author = 'Randall Degges',
+    author_email = 'rdegges@gmail.com',
+    license = 'UNLICENSE',
+    url = 'https://github.com/rdegges/django-heroku-postgresify',
+    keywords = 'django heroku cloud postgresql postgres db database awesome epic',
+    description = 'Automatic Django database configuration on Heroku.',
+    long_description = open(normpath(join(dirname(abspath(__file__)),
+        'README.md'))).read()
+
+)
