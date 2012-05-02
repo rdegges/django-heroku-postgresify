04984c03c1fc4ac820e22da9842729da92f82835
Randall Degges <rdegges@gmail.com>
2012-05-01 19:54:34 -0700

    Adding some basic sanity tests for detecting DB URLs.

--- tests.py
+++ tests.py
@@ -1,7 +1,31 @@
+from os import environ
 from unittest import TestCase
 
 from postgresify import postgresify
 
 
 class Postgresify(TestCase):
-    pass
+
+    def test_returns_empty_dict_if_no_dbs_are_available(self):
+        self.assertEqual(postgresify(), {})
+
+    def test_detects_database_url(self):
+        environ['DATABASE_URL'] = 'postgres://abc:def@ec2-23-23-217-149.compute-1.amazonaws.com/xxx'
+        self.assertTrue(postgresify()['default'])
+        del environ['DATABASE_URL']
+
+    def test_detects_shared_database(self):
+        environ['SHARED_DATABASE_URL'] = 'postgres://abc:def@ec2-23-23-217-149.compute-1.amazonaws.com/xxx'
+        self.assertTrue(postgresify()['SHARED_DATABASE'])
+        del environ['SHARED_DATABASE_URL']
+
+    def test_detects_heroku_postgres_shared_database(self):
+        environ['HEROKU_SHARED_POSTGRESQL_RED_URL'] = 'postgres://abc:def@ec2-23-23-217-149.compute-1.amazonaws.com/xxx'
+        self.assertTrue(postgresify()['SHARED_RED'])
+        del environ['HEROKU_SHARED_POSTGRESQL_RED_URL']
+
+    def test_detects_heroku_databases(self):
+        environ['HEROKU_POSTGRESQL_TEAL_URL'] = 'postgres://abc:def@ec2-23-23-217-149.compute-1.amazonaws.com/xxx'
+        self.assertTrue(postgresify()['TEAL'])
+        del environ['HEROKU_POSTGRESQL_TEAL_URL']
+
