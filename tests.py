2a877968b33bb34b3cca3453bd134fe1b102e6d9
Randall Degges <rdegges@gmail.com>
2012-05-01 20:01:17 -0700

    Adding another test for ensuring we don't do duplicates.

--- tests.py
+++ tests.py
@@ -29,3 +29,10 @@ class Postgresify(TestCase):
         self.assertTrue(postgresify()['TEAL'])
         del environ['HEROKU_POSTGRESQL_TEAL_URL']
 
+    def test_doesnt_include_a_database_twice_if_it_is_set_as_default(self):
+        environ['DATABASE_URL'] = 'postgres://abc:def@ec2-23-23-217-149.compute-1.amazonaws.com/xxx'
+        environ['HEROKU_POSTGRESQL_AQUA_URL'] = 'postgres://abc:def@ec2-23-23-217-149.compute-1.amazonaws.com/xxx'
+        self.assertFalse('AQUA' in postgresify())
+        del environ['DATABASE_URL']
+        del environ['HEROKU_POSTGRESQL_AQUA_URL']
+
